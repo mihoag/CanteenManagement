74657c122882d053c19897b170c4a4111cfeb73d
21120420 <lehuynhcuong0701@gmail.com>
2024-01-05 00:31:11 +0700

    add payment

--- controllers/userController/orderController.c.js
+++ controllers/userController/orderController.c.js
@@ -1,25 +1,59 @@
-const orderUser = require('../../model/orderUser.m')
+const orderUser = require("../../model/orderUser.m");
 const { pgp, db } = require("../../configs/DBconnection");
 class orderController {
-    async showOrder(req, res, next) {
-        try {
-            let listOrder = await orderUser.getAll();
-            res.render("user/order", { layout: "userLayout", username: req.session.username, listOrder: listOrder });
-        } catch (error) {
-            next(error);
-        }
+  async showOrder(req, res, next) {
+    try {
+      let listOrder = await orderUser.getAll();
+      res.render("user/order", {
+        layout: "userLayout",
+        username: req.session.username,
+        listOrder: listOrder,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async Orderdetail(req, res, next) {
-        try {
-            let id = req.body.id;
-            //console.log(id);
-            let data = await db.any('select o.*, i."price", i."image", i."name" from "orderdetail" o , "item" i  where o.id_item = i.id_item and o.id_order = $1', [id]);
-            res.status(200).json({ listDetail: data });
-        } catch (error) {
-            res.status(400).json({ msg: "Error" });
-        }
+  async Orderdetail(req, res, next) {
+    try {
+      let id = req.body.id;
+      //console.log(id);
+      let data = await db.any(
+        'select o.*, i."price", i."image", i."name" from "orderdetail" o , "item" i  where o.id_item = i.id_item and o.id_order = $1',
+        [id]
+      );
+      res.status(200).json({ listDetail: data });
+    } catch (error) {
+      res.status(400).json({ msg: "Error" });
     }
+  }
 
+  async addOrder(req, res, next) {
+    try {
+      //   console.log(req.body);
+      const username = req.session.username;
+      const order = await orderUser.addOrder({ ...req.body, username });
+      //   console.log(order);
+
+      const { id_order } = order[0];
+      const { item_ids, quantities } = req.body;
+      const orderDetails = await Promise.all(
+        item_ids.map(async (item_id, index) => {
+          return await orderUser.addOrderDetail({
+            id_order,
+            item_id,
+            quantity: quantities[index],
+          });
+        })
+      );
+      //   console.log(orderDetails);
+      res.status(200).json({
+        status: "success",
+        data: { order, orderDetails },
+      });
+    } catch (err) {
+      res.status(400).json({ msg: "Error" });
+    }
+  }
 }
-module.exports = new orderController;
+module.exports = new orderController();
