595661c62da9c303af835f863695a20b48ddcc5c
21120420 <lehuynhcuong0701@gmail.com>
2024-01-06 00:28:41 +0700

    fix

--- model/cart.m.js
+++ model/cart.m.js
@@ -34,7 +34,7 @@ module.exports = {
       const user = await db.selectByOneField("user", "username", username);
       const id_user = user[0].id_user;
       const data = await db.query(
-        `SELECT i.*,c."quantity" FROM "cart" c,"item" i where c."id_user" = '${id_user}' and c."id_item" = i."id_item"`
+        `SELECT i."image", i."name", i."type", i."saleprice", i."id_item", c."id_cart", c."quantity" FROM "cart" c,"item" i where c."id_user" = '${id_user}' and c."id_item" = i."id_item"`
       );
       // console.log(data);
       return data;
