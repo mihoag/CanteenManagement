172736f2571c5bbf84ed29fdd520446d01930a5e
21120447 <haonhat2729@gmail.com>
2024-01-05 10:21:21 +0700

    export excel

--- helper/hbs_helper.js
+++ helper/hbs_helper.js
@@ -1,15 +1,15 @@
 
 module.exports = {
-    ifEquals: function (arg1, arg2, options) {
-      return arg1 == arg2 ? options.fn(this) : options.inverse(this);
-    },
-    times: function (n, options) {  
-      var accum = "";
-      for (var i = 0; i < n; ++i)
-       accum += options.fn(i + 1);
-      return accum;
-    },
-    ifBig: function (arg1, arg2, options) {
-      return arg1 >= arg2 ? options.fn(this) : options.inverse(this);
-    },
-  };
+  ifEquals: function (arg1, arg2, options) {
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+  },
+  times: function (n, options) {
+    var accum = "";
+    for (var i = 0; i < n; ++i)
+      accum += options.fn(i + 1);
+    return accum;
+  },
+  ifBig: function (arg1, arg2, options) {
+    return arg1 >= arg2 ? options.fn(this) : options.inverse(this);
+  },
+};
