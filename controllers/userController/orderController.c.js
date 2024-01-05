58241333348fe4621e709197c7011694e4317a98
21120420 <lehuynhcuong0701@gmail.com>
2024-01-05 11:37:41 +0700

    Update

--- controllers/userController/orderController.c.js
+++ controllers/userController/orderController.c.js
@@ -1,4 +1,6 @@
 const orderUser = require("../../model/orderUser.m");
+const userModel = require("../../model/user.m");
+const itemModel = require("../../model/item.m");
 const { pgp, db } = require("../../configs/DBconnection");
 class orderController {
   async showOrder(req, res, next) {
@@ -32,15 +34,39 @@ class orderController {
     try {
       //   console.log(req.body);
       const { item_ids, quantities, total_price } = req.body;
-      // Check if total_price < account balance
-
       const username = req.session.username;
-      const order = await orderUser.addOrder({ ...req.body, username });
-      //   console.log(order);
 
-      const { id_order } = order[0];
+      let user = await userModel.selectByUsername(username);
+      const { id_user, money } = user;
+      // Check if total_price < account balance
+      let err;
+      if (total_price > money) {
+        err = new Error("Số dư trong tài khoản không đủ ! Thanh toán thất bại");
+        err.isOperational = true;
+        throw err;
+      } else {
+        user.money = money - total_price;
+        user = await userModel.updateProfile(user);
+      }
 
       // Check if quantity satifies item stock
+      for (let i = 0; i < item_ids.length; i++) {
+        const { quantity } = await itemModel.getItemQuantity(item_ids[i]);
+        if (quantity < quantities[i]) {
+          err = new Error("Không đủ hàng");
+          err.isOperational = true;
+          throw err;
+        } else {
+          await itemModel.updateItemQuantity(
+            item_ids[i],
+            quantity - quantities[i]
+          );
+        }
+      }
+
+      const order = await orderUser.addOrder({ ...req.body, id_user });
+      //   console.log(order);
+      const { id_order } = order[0];
 
       const orderDetails = await Promise.all(
         item_ids.map(async (item_id, index) => {
@@ -57,7 +83,9 @@ class orderController {
         data: { order, orderDetails },
       });
     } catch (err) {
-      res.status(400).json({ msg: "Error" });
+      res
+        .status(400)
+        .json({ errMsg: err.message, isOperational: err.isOperational });
     }
   }
 }
---
+++ model/item.m.js
@@ -0,0 +1,30 @@
+const db = require("../db/db");
+
+module.exports = {
+  getItemQuantity: async (itemId) => {
+    try {
+      const data = await db.query(
+        `SELECT "quantity" FROM "item" where "item"."id_item" = ${itemId}`
+      );
+      // console.log(data);
+      return data[0];
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateItemQuantity: async (item_id, quantity) => {
+    try {
+      const data = await db.query(
+        `UPDATE item
+        SET quantity = ${quantity} 
+        WHERE id_item = ${item_id}
+        returning *`
+      );
+      console.log(data);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
